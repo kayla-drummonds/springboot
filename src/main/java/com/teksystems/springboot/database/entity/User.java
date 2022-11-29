4a730864f2c0e267c438ae33fcb0ba847f85d05b
Michaela Drummonds <mickeysymone@gmail.com>
2022-11-29 15:05:10 -0500

    new User class, erased error log

---
+++ src/main/java/com/teksystems/springboot/database/entity/User.java
@@ -0,0 +1,61 @@
+package com.teksystems.springboot.database.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column(name = "create_date", nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Column
+    private String address;
+
+    @Column
+    private String city;
+
+    @Column
+    private String state;
+
+    @Column
+    private Integer zip;
+
+    @Column
+    private String phone;
+}
