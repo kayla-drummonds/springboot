ca2c644d6ce002297881bd5fbb248b91b421304c
Michaela Drummonds <mickeysymone@gmail.com>
2022-12-05 11:52:23 -0500

    changed repository to RepositoryRestResource

--- src/main/java/com/teksystems/springboot/database/dao/TutorialRepository.java
+++ src/main/java/com/teksystems/springboot/database/dao/TutorialRepository.java
@@ -1,11 +1,14 @@
 package com.teksystems.springboot.database.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.teksystems.springboot.database.entity.Tutorial;
 
-@Repository
+@RepositoryRestResource(path = "learningtutorials")
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
 
+    List<Tutorial> findByPublished(boolean published);
 }
