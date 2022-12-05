623113e163e124fa44fdeedb0a16b47c3ebabab3
Michaela Drummonds <mickeysymone@gmail.com>
2022-12-05 11:37:43 -0500

    added tutorial entity

---
+++ src/main/java/com/teksystems/springboot/database/entity/Tutorial.java
@@ -0,0 +1,36 @@
+package com.teksystems.springboot.database.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name = "tutorials")
+public class Tutorial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "published")
+    private boolean published;
+}
